c7e68b5839de68ec7abc24632e10964b5244e146
Kaue Fulgencio <Kauemfulgencio@outlook.com>
2023-07-18 11:40:21 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import SignIn from './src/pages/SignIn'
+import Navigator from './src/components/navbar/navigator'
 
 export default function App() {
   return (
+    /*
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Projeto React Native!</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+      <SignIn/>
+      <Navigator/>
+    </View>*/
+    <Navigator/>
   );
 }
 
@@ -17,4 +29,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
 });
---
+++ src/components/navbar/navigator.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AirplaneOutline  from '@expo/vector-icons/Ionicons';
+
+
+<AirplaneOutline
+  color={'#00000'} 
+  title={''}
+  height="250px"
+  width="250px"
+/>
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+
+
+export default function Navigator() {
+  return (
+    
+    <NavigationContainer>
+      <Tab.Navigator>
+
+        <AirplaneOutline/>
+
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+        
+        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} />
+        
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
+});
