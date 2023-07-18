03f6c7690ff3acef3b019a703f7250392575d1e9
Kaue Fulgencio <Kauemfulgencio@outlook.com>
2023-07-18 12:46:47 -0300

    adicionando rotas de autenticação

---
+++ src/pages/ChatIn/index,tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ChatIn(){
+    return(
+        <View>
+            <Text>
+                Tela de Login
+            </Text>
+        </View>
+    )
+}
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native'
+import { createNavigationContainerRef } from '@react-navigation/native';
+
+const Stack = createNavigationContainerRef();
+
+function AppRoutes(){
+  return(
+    <Text></Text>
+  )
+}
+
+export default AppRoutes;
+
